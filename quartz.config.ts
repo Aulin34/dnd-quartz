5f1321ab4cfbeaaa2f62a6b944c3b32b1f9806b2
Colin McCoy <colinmccoy007@gmail.com>
2026-02-21 14:38:38 -0600

    Quartz sync: Feb 21, 2026, 2:38 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+	tagId: "G-HENPF4776Q",
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
